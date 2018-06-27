cb277f41d03c206efbfc6057c59e2c3da11fe97f
gjtest12 <barbuj@yahoo.com>
2018-06-27 01:27:42 -0400

    Adding Mantis download to build process

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Installing MBT version ${VER}"
-touch mbt_${VER}.zip
+echo "Installing Mantis ${VER}"
+
+wget -O mantisbt-${VER}.zip https://sourceforge.net/projects/mantisbt/files/mantis-stable/${VER}/mantisbt-${VER}.zip/download
 
