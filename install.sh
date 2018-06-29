31d043fe3d24ae6028cb28c22e5d7fe78699ae37
gjtest12 <barbuj@yahoo.com>
2018-06-28 23:29:28 -0400

    A few more tweaks

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 
 echo "Installing pre-requisites"
 sudo apt-get update -q
-sudo apt-get install unzip -y -q
 sudo apt-get install php-mbstring -y
 sudo apt-get install apache2 -y
-sudo apt-get install php-mysql -y
+sudo apt-get install php5-mysql -y
 sudo service apache2 restart
 
 echo "Creating Mantis database"
@@ -19,7 +18,7 @@ wget -q -O mantisbt-${VER}.zip https://sourceforge.net/projects/mantisbt/files/m
 unzip mantisbt-${VER}.zip > /dev/null
 sudo mv ./mantisbt-${VER} /var/www/html/mantis/
 
-ls /var/www/html
+ls -al /var/www/html
 
 echo "Configuring Mantis"
 curl --request POST "http://127.0.0.1/mantis/admin/install.php" -d "install=2&db_type=mysqli&hostname=127.0.0.1&db_username=mantis&db_password=m4nt1s&database_name=mantis&admin_username=&admin_password=&db_table_prefix=mantis&db_table_plugin_prefix=plugin&db_table_suffix=_table&timezone=UTC"
@@ -35,5 +34,5 @@ echo '$g_crypto_master_salt = '"'XYtly+fyYaWPnscCvj0PkCFPMuy5hii1VebD2oamyPw=';"
 
 echo "Verifying install"
 wget http://127.0.0.1/mantis > mantis_install_test.txt
-cat mantis_install_test.txt
+ls -al
 
