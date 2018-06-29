33061037b6225a8d8b1e922cb64993227820aef4
gjtest12 <barbuj@yahoo.com>
2018-06-28 23:06:56 -0400

    Expanding install script

--- install.sh
+++ install.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
-echo "Installing Mantis ${VER}"
 
-wget -O mantisbt-${VER}.zip https://sourceforge.net/projects/mantisbt/files/mantis-stable/${VER}/mantisbt-${VER}.zip/download
+echo "Installing pre-requisites"
+sudo apt-get update
+sudo apt-get install unzip -y
+sudo apt-get install php-mbstring -y
+sudo apt-get install apache2 -y
+sudo apt-get install php-mysql -y
+
+echo "Creating Mantis database"
+sudo mysql -u root -e "CREATE DATABASE mantis;"
+sudo mysql -u root -e "CREATE USER 'mantis'@'localhost' IDENTIFIED BY 'm4nt1s';"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON mantis.* to 'mantis'@'localhost';"
+sudo mysql -u root -e "FLUSH PRIVILEGES;"
+
+echo "Downloading Mantis"
+wget -O mantisbt-${VER}.zip https://sourceforge.net/projects/mantisbt/files/mantis-stable/${VER}/mantisbt-${VER}.zip/download > /dev/null
+unzip mantisbt-${VER}.zip > /dev/null
+sudo mv ./mantisbt-${VER}.zip /var/www/html/mantis
+
+echo "Configuring Mantis"
+curl --request POST "http://127.0.0.1/mantis/admin/install.php" -d "install=2&db_type=mysqli&hostname=127.0.0.1&db_username=mantis&db_password=m4nt1s&database_name=mantis&admin_username=&admin_password=&db_table_prefix=mantis&db_table_plugin_prefix=plugin&db_table_suffix=_table&timezone=UTC"
+
+echo "<?php" > /var/www/html/mantis/config/config.php
+echo '$g_hostname = '"'127.0.0.1';" >> /var/www/html/mantis/config/config.php
+echo '$g_db_type = '"'mysqli';" >> /var/www/html/mantis/config/config.php
+echo '$g_database_name = '"'mantis';" >> /var/www/html/mantis/config/config.php
+echo '$g_db_username = '"'mantis';" >> /var/www/html/mantis/config/config.php
+echo '$g_db_password = '"'m4ntis';" >> /var/www/html/mantis/config/config.php
+echo '$g_default_timezone = '"'UTC';" >> /var/www/html/mantis/config/config.php
+echo '$g_crypto_master_salt = '"'XYtly+fyYaWPnscCvj0PkCFPMuy5hii1VebD2oamyPw=';" >> /var/www/html/mantis/config/config.php
+
+echo "Verifying install"
+wget http://127.0.0.1/mantis
 
