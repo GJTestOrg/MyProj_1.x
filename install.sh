6299ec7d4b5ea58c794d055d51e4815cd5046bdf
gjtest12 <barbuj@yahoo.com>
2018-06-29 00:00:31 -0400

    More tweaks

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 echo "Installing pre-requisites"
 sudo apt-get update > /dev/null 2>&1
-sudo apt-get install apache2 -y -q
-sudo apt-get install php5-mysql -y -q
+sudo apt-get install apache2 -y -q > /dev/null 2>&1
+sudo apt-get install php5-mysql -y -q /dev/null 2>&1
 sudo apt-get install php-mbstring -y
 sudo service apache2 restart > /dev/null 2>&1
 
@@ -18,25 +18,25 @@ wget -q -O mantisbt-${VER}.zip https://sourceforge.net/projects/mantisbt/files/m
 unzip mantisbt-${VER}.zip > /dev/null
 sudo mv ./mantisbt-${VER} /var/www/html/mantis/
 
-ls -al /var/www/html
+sudo ls -al /var/www/html
 
 echo "Configuring Mantis"
 curl --request POST "http://127.0.0.1/mantis/admin/install.php" -d "install=2&db_type=mysqli&hostname=127.0.0.1&db_username=mantis&db_password=m4nt1s&database_name=mantis&admin_username=&admin_password=&db_table_prefix=mantis&db_table_plugin_prefix=plugin&db_table_suffix=_table&timezone=UTC" > /dev/null 2>&1
 
-echo "<?php" > /var/www/html/mantis/config/config.php
-echo '$g_hostname = '"'127.0.0.1';" >> /var/www/html/mantis/config/config.php
-echo '$g_db_type = '"'mysqli';" >> /var/www/html/mantis/config/config.php
-echo '$g_database_name = '"'mantis';" >> /var/www/html/mantis/config/config.php
-echo '$g_db_username = '"'mantis';" >> /var/www/html/mantis/config/config.php
-echo '$g_db_password = '"'m4ntis';" >> /var/www/html/mantis/config/config.php
-echo '$g_default_timezone = '"'UTC';" >> /var/www/html/mantis/config/config.php
-echo '$g_crypto_master_salt = '"'XYtly+fyYaWPnscCvj0PkCFPMuy5hii1VebD2oamyPw=';" >> /var/www/html/mantis/config/config.php
+echo "<?php" > /var/www/html/mantis/config/config_inc.php
+echo '$g_hostname = '"'127.0.0.1';" >> /var/www/html/mantis/config/config_inc.php
+echo '$g_db_type = '"'mysqli';" >> /var/www/html/mantis/config/config_inc.php
+echo '$g_database_name = '"'mantis';" >> /var/www/html/mantis/config/config_inc.php
+echo '$g_db_username = '"'mantis';" >> /var/www/html/mantis/config/config_inc.php
+echo '$g_db_password = '"'m4ntis';" >> /var/www/html/mantis/config/config_inc.php
+echo '$g_default_timezone = '"'UTC';" >> /var/www/html/mantis/config/config_inc.php
+echo '$g_crypto_master_salt = '"'XYtly+fyYaWPnscCvj0PkCFPMuy5hii1VebD2oamyPw=';" >> /var/www/html/mantis/config/config_inc.php
 
 echo "Verifying configuration"
-cat /var/www/html/mantis/config/config_inc.php
+sudo cat /var/www/html/mantis/config/config_inc.php
 
 echo "Verifying install"
-wget http://127.0.0.1/mantis/ > mantis_install_test.txt
+wget http://127.0.0.1/mantis/login_page.php > mantis_install_test.txt
 ls -al
 cat mantis_install_test.txt
 
